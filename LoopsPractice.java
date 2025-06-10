14789e8881157b28bd346db683e6cb733ae71202
kishanmeta4 <122509500+kishanmeta4@users.noreply.github.com>
2025-06-10 20:20:41 +0530

    Add files via upload

---
+++ LoopsPractice.java
@@ -0,0 +1,18 @@
+package myFirstPackage;
+
+public class LoopsPractice {
+	public static void main(String[] args) {
+		
+		
+		int num=5;
+		for(int i=1; i<=100; i++) {
+			if (i%num==0) {
+				continue;
+				
+			}
+			System.out.println(i);	
+
+			}
+	}
+
+}
