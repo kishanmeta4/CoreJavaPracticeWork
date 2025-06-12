eff52c1756191ac341601bcf1bc511f63c53380c
kishanmeta4 <122509500+kishanmeta4@users.noreply.github.com>
2025-06-12 21:49:19 +0530

    Add files via upload

---
+++ MultiplicationTable.java
@@ -0,0 +1,18 @@
+package myFirstPackage;
+
+public class MultiplicationTable {
+	
+	public static void main (String[] args) {
+		
+		int num =12;
+		int max=10;		 
+		
+		for (int i=1; i<=max;i++) {
+			
+			
+			System.out.println(num +" * "+i+" = "+(num*i));
+			
+		}
+	}
+
+}
